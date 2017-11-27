dac971d2fcfcb14022327a7e979702d823ce8b6f
Herman Rogers <herman.rogers@gmail.com>
2017-11-27 10:18:18 -0500

    Add notes from november

--- create-lab-note.sh
+++ create-lab-note.sh
@@ -24,7 +24,7 @@ else
   printf "Creating new labnote \n"
   touch ${labNoteBook}/$labNote
   printf "Adding format to labnote \n"
-  printf "# ${currentMonthName} `date +%d,%Y`\n\n\n## Ideas:\n\n### Personal\n\n### Work\n\n## Review:\n\n## Notes:" > ${labNoteBook}/$labNote
+  printf "# ${currentMonthName} `date +%d,%Y`\n\n\n## Ideas:\n\n## Review:\n\n## Notes:\n\n### Personal\n\n### Work\n\n" > ${labNoteBook}/$labNote
   printf "Labnote $labNote created in $labNoteBook"
 fi
 
